e1aa95341f852b26c3db512d192c2c9edcb27281
Gareth Jones <garetrhsion@googlemail.com>
2019-08-03 11:17:45 +0100

    Fixed the problem with the simulation. I had to reduce tan_d to remove the attenuation. This is possibly a problem which needs investigation, but the simulated data roughly matches the experimental data

--- ResonatorOptimizer/bragg/bragg.py
+++ ResonatorOptimizer/bragg/bragg.py
@@ -1,3 +1,49 @@
+# import ResonatorOptimizer as ro
+# import numpy as np
+# import pandas as pd
+
+# class Bragg:
+#     def __init__(self,fo,er,h,t,pen_depth,no_mirrors=5):
+#         self.__fo = fo
+#         self.__er = er
+#         self.__h = h
+#         self.__t = t
+#         self.__pen_depth = pen_depth
+#         self.__no_mirrors = no_mirrors
+#         return
+    
+#     def get_abcd(self,cpw,freq,length):
+        
+#         tl = ro.TLParameters(width=cpw.print_cpw_params().width.values[0],
+#             gap=cpw.print_cpw_params().gap.values[0],
+#             er=self.__er,h=self.__h,t=self.__t,pen_depth=self.__pen_depth)
+        
+#         gamma = tl.gamma(freq,tan_d=0.005)
+#         Z0 = cpw.impedance_total()
+
+#         sp = ro.Sparams(freq=freq,gamma=gamma,length=length,Z0=Z0)
+#         abcd = sp.transmission()
+#         return abcd
+
+#     def cpw_section(self,width,gap,length,elen=180):
+#         cpw = ro.CPW(width=width,gap=gap,length=length,elen=elen,fo=self.__fo, er=self.__er, h=self.__h, t=self.__t, pen_depth=self.__pen_depth)
+#         return cpw
+
+#     def bragg_resonator(self,freq,lowZ,highZ,cavity):  
+#         cav_abcd = self.get_abcd(cavity,freq,cavity.print_cpw_params().length.values[0])
+#         lowZ_abcd = self.get_abcd(lowZ,freq,lowZ.print_cpw_params().length.values[0])
+#         highZ_abcd = self.get_abcd(highZ,freq,highZ.print_cpw_params().length.values[0])
+#         mirror_lhs = [np.matmul(lowZ_abcd[i], highZ_abcd[i]) for i in range(len(freq))]
+#         mirror_rhs = [np.matmul(highZ_abcd[i], lowZ_abcd[i]) for i in range(len(freq))]
+#         bragg = [np.matmul(mirror_lhs[i]**self.__no_mirrors, np.matmul(cav_abcd[i], 
+#         	mirror_rhs[i]**self.__no_mirrors)) for i in range(len(freq))]
+
+#         sp = ro.Sparams(freq=freq)
+#         bragg_s21 = sp.s21_from_abcd(bragg)
+
+#         return bragg_s21
+
+
 import ResonatorOptimizer as ro
 import numpy as np
 import pandas as pd
@@ -13,32 +59,27 @@ class Bragg:
         return
     
     def get_abcd(self,cpw,freq,length):
-        
-        tl = ro.TLParameters(width=cpw.print_cpw_params().width.values[0],
-            gap=cpw.print_cpw_params().gap.values[0],
-            er=self.__er,h=self.__h,t=self.__t,pen_depth=self.__pen_depth)
-        
-        gamma = tl.gamma(freq,tan_d=0.005)
-        Z0 = cpw.impedance_total()
+        gamma = cpw.gamma(freq,tan_d=1e-07)
+        Z0 = cpw.impedance_kinetic()
 
         sp = ro.Sparams(freq=freq,gamma=gamma,length=length,Z0=Z0)
         abcd = sp.transmission()
         return abcd
 
-    def cpw_section(self,width,gap,length,elen=180):
-        cpw = ro.CPW(width=width,gap=gap,length=length,elen=elen,fo=self.__fo, er=self.__er, h=self.__h, t=self.__t, pen_depth=self.__pen_depth)
+    def cpw_section(self,width, gap, length):
+        cpw = ro.cpwCalcs(width=width,gap=gap,length=length,fo=self.__fo, er=self.__er, h=self.__h, t=self.__t, pen_depth=self.__pen_depth)
         return cpw
 
-    def bragg_resonator(self,freq,lowZ,highZ,cavity):  
+    def bragg_resonator(self,freq,lowZ, highZ, cavity):  
         cav_abcd = self.get_abcd(cavity,freq,cavity.print_cpw_params().length.values[0])
         lowZ_abcd = self.get_abcd(lowZ,freq,lowZ.print_cpw_params().length.values[0])
         highZ_abcd = self.get_abcd(highZ,freq,highZ.print_cpw_params().length.values[0])
+            
         mirror_lhs = [np.matmul(lowZ_abcd[i], highZ_abcd[i]) for i in range(len(freq))]
         mirror_rhs = [np.matmul(highZ_abcd[i], lowZ_abcd[i]) for i in range(len(freq))]
         bragg = [np.matmul(mirror_lhs[i]**self.__no_mirrors, np.matmul(cav_abcd[i], 
-        	mirror_rhs[i]**self.__no_mirrors)) for i in range(len(freq))]
+            mirror_rhs[i]**self.__no_mirrors)) for i in range(len(freq))]
 
         sp = ro.Sparams(freq=freq)
         bragg_s21 = sp.s21_from_abcd(bragg)
-
         return bragg_s21
